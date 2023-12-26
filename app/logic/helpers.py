57832fd112a2e2984835f209267070af63d1cbd1
SanchezFelipe01 <vegafelipe015@gmail.com>
2023-12-25 23:54:40 -0500

    Helpers modifications

--- app/logic/helpers.py
+++ app/logic/helpers.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sqlalchemy.sql import text
 from sqlalchemy import inspect
 import csv
 from flask import render_template, jsonify
@@ -212,74 +211,105 @@ def delete_all_employees(db, Employee):
 
 # REQUIREMENT 1
     
-def recruitments_by_quarter(hired_employees, departments, jobs):
+def recruitments_by_quarter(db, Department, Job, Employee):
 
-    hired_employees['datetime'] = pd.to_datetime(hired_employees['datetime'])
-    hired_employees_2021 = hired_employees[hired_employees['datetime'].dt.year == 2021]
+    columns = ['department', 'job', 'Q1', 'Q2', 'Q3', 'Q4']
+    df = pd.DataFrame(columns=columns)
 
-    hired_employees_2021['quarter'] = hired_employees_2021['datetime'].dt.quarter
+    try:
+        departments = pd.read_sql_table(Department.__tablename__, db.engine)
+        jobs = pd.read_sql_table(Job.__tablename__, db.engine)
+        hired_employees = pd.read_sql_table(Employee.__tablename__, db.engine)
 
-    merged_df = pd.merge(
-        hired_employees_2021, 
-        departments, 
-        left_on='department_id', 
-        right_on='id'
-    )
-    
-    merged_df = pd.merge(
-        merged_df, 
-        jobs, 
-        left_on='job_id', 
-        right_on='id'
-    )
-    
-    result_df = (
-        merged_df.groupby(['department', 'job', 'quarter'])
-            .size().unstack(fill_value=0).reset_index()
-    )
+        if hired_employees.empty or departments.empty or jobs.empty:
+            return render_template('result.html', result=df, title='Recruitments per quarter')
 
-    result_df.columns = ['department', 'job', 'Q1', 'Q2', 'Q3', 'Q4']
+        hired_employees['datetime'] = pd.to_datetime(hired_employees['datetime'])
+        hired_employees_2021 = hired_employees[hired_employees['datetime'].dt.year == 2021]
 
-    result_df = (
-        result_df.sort_values(by=['department', 'job'])
-            .reset_index(drop=True)
-    )
+        hired_employees_2021['quarter'] = hired_employees_2021['datetime'].dt.quarter
 
-    result_df.index = result_df.index +1
-    return result_df
+        merged_df = pd.merge(
+            hired_employees_2021, 
+            departments, 
+            left_on='department_id', 
+            right_on='id'
+        )
+        
+        merged_df = pd.merge(
+            merged_df, 
+            jobs, 
+            left_on='job_id', 
+            right_on='id'
+        )
+        
+        result_df = (
+            merged_df.groupby(['department', 'job', 'quarter'])
+                .size().unstack(fill_value=0).reset_index()
+        )
+
+        result_df.columns = ['department', 'job', 'Q1', 'Q2', 'Q3', 'Q4']
+
+        result_df = (
+            result_df.sort_values(by=['department', 'job'])
+                .reset_index(drop=True)
+        )
+
+        result_df.index = result_df.index +1
+        return render_template('result.html', result=result_df, title='Recruitments per quarter')
+    except Exception:
+        df.loc[0] = 'Error'
+        return render_template('result.html', result=df, title='Recruitments per quarter')
 
 # REQUIREMENT 1
 
 # REQUIREMENT 2
 
-def people_by_department(hired_employees, departments):
+def people_by_department(db, Department, Employee):
 
-    hired_employees['datetime'] = pd.to_datetime(hired_employees['datetime'])
-    hired_employees_2021 = hired_employees[hired_employees['datetime'].dt.year == 2021]
+    columns = ['id', 'department', 'hired']
+    df = pd.DataFrame(columns=columns)
 
-    merged_df = pd.merge(
-        hired_employees_2021, 
-        departments, 
-        left_on='department_id', 
-        right_on='id')
+    try:
 
-    merge2 = (
-        merged_df.groupby(['id_y', 'department'])
-            .size().reset_index(name='hired')
-    )
-    
-    avg_num_employees_hired = merge2['hired'].mean()
+        departments = pd.read_sql_table(Department.__tablename__, db.engine)
+        hired_employees = pd.read_sql_table(Employee.__tablename__, db.engine)
 
-    people_by_department_df = merge2[merge2['hired'] > avg_num_employees_hired]
+        if hired_employees.empty or departments.empty:
+            columns = ['id', 'department', 'hired']
+            df = pd.DataFrame(columns=columns)
+            return render_template('result.html', result=df, title='People by department')
 
-    people_by_department_df = (
-        people_by_department_df.sort_values(by='hired', ascending=False)
-            .reset_index(drop=True)
-    )
+        hired_employees['datetime'] = pd.to_datetime(hired_employees['datetime'])
+        hired_employees_2021 = hired_employees[hired_employees['datetime'].dt.year == 2021]
 
-    people_by_department_df.rename(columns={'id_y': 'id'}, inplace=True)
-    people_by_department_df.index = people_by_department_df.index +1
-    return people_by_department_df
+        merged_df = pd.merge(
+            hired_employees_2021, 
+            departments, 
+            left_on='department_id', 
+            right_on='id')
+
+        merge2 = (
+            merged_df.groupby(['id_y', 'department'])
+                .size().reset_index(name='hired')
+        )
+        
+        avg_num_employees_hired = merge2['hired'].mean()
+
+        people_by_department_df = merge2[merge2['hired'] > avg_num_employees_hired]
+
+        people_by_department_df = (
+            people_by_department_df.sort_values(by='hired', ascending=False)
+                .reset_index(drop=True)
+        )
+
+        people_by_department_df.rename(columns={'id_y': 'id'}, inplace=True)
+        people_by_department_df.index = people_by_department_df.index +1
+        return render_template('result.html', result=people_by_department_df, title='People by department')
+
+    except Exception:
+        df.loc[0] = 'Error'
+        return render_template('result.html', result=df, title='People by department')
 
 # REQUIREMENT 2
 
